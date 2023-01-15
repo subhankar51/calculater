b6400ee9655ebaaa19b8b57fc0658aa5e3ac4812
vagrant <vagrant@deploy>
2023-01-15 19:14:18 +0000

    added

--- Django Calculator App/Calculator/settings.py
+++ Django Calculator App/Calculator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'esw#4jd1o2=w#f83_n!%e%)xncniao#$we@3!1)-6!_gp@*y&('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.16.1.102']
 
 
 # Application definition
